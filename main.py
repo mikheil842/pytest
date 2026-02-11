d813a10df063a31fb0ebfca6f9a124bf266a48ec
mikheil842 <mikheil842@telaveli.ge>
2026-02-11 08:24:55 +0400

    გავაკეთე ფანჯარა

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("ჩემი პირველი პროგრამის სათაური")
+
+root.geometry("800x600")
+
+label = tk.Label(root, text="გამარჯობა! მე მქვია მიშო.", font=("arial",34))
+label.pack()
+
+
+root.mainloop()
+
